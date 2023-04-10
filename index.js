14b7e4e83f7ba4e8738d73a624885a9f215e29ea
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-04-09 22:16:43 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+function findMatching(driverList, name) {
+    return driverList.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(driverList, partialName) {
+    return driverList.filter(driver => {
+        const array = {value: true};
+        for (const index in partialName) {
+            if (partialName[index] === driver[index]) {
+                array.value = true;
+            } else {
+                array.value = false;
+                break;
+            }
+        }
+        if (array.value) {
+            return driver;
+        }
+    });
+}
+
+function matchName(driverList, nameInput) {
+    return driverList.filter(driverObj => driverObj.name.toLowerCase() === nameInput.toLowerCase());
+}
